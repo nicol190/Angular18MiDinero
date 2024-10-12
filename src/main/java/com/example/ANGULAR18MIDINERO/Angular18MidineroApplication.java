5f55d64a16b239125d205091e62d8f91c62c8b9c
nicol190 <75269902+nicol190@users.noreply.github.com>
2024-10-12 15:20:54 -0500

    Nuevo proyecto
    Hola, se sube nuevo proyecto

---
+++ src/main/java/com/example/ANGULAR18MIDINERO/Angular18MidineroApplication.java
@@ -0,0 +1,13 @@
+package com.example.ANGULAR18MIDINERO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular18MidineroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular18MidineroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ANGULAR18MIDINERO/Angular18MidineroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ANGULAR18MIDINERO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Angular18MidineroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
